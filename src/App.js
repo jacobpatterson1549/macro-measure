638770501872effa4d1119fae50d85dc2eb74c91
Jacob Patterson <jacob.patterson@gmail.com>
2021-05-05 10:27:35 -0700

    removing generated code, adding Header component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Header />
 }
 
 export default App;
+
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: props.view || 'groups',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <span onClick={() => { this.setState({view: 'groups'}); }} title="home">Macro Measure</span>
+          <span onClick={() => { this.setState({view: 'about'}); }} title="About">ⓘ</span>
+          <span onClick={() => { this.setState({view: 'help'}); }} title="Help">?</span>
+          <span onClick={() => { this.setState({view: 'settings'}); }} title="Settings">⚙</span>
+        </header>
+        <main>
+          <Main view={this.state.view} />
+        </main>
+      </div>
+    );
+  }
+}
+
+class Main extends React.Component {
+  render() {
+    return (
+      <p>TODO: {this.props.view}</p>
+    );
+  };
+}
