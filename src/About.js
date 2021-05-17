96093ea6771ae403c1ec05474cd2f28959015c7b
Jacob Patterson <jacob.patterson@gmail.com>
2021-05-17 08:31:00 -0700

    fixing github link

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ export const About = () => (
         <h1>About Macro Measure</h1>
         <ul>
             <li>© 2021 Jacob Patterson</li>
-            <li><a href="https://github.com/jacobpatterson1549/selene-bananas">Github</a></li>
+            <li><a href="https://github.com/jacobpatterson1549/macro-measure">Github</a></li>
             <li><a href="https://www.linkedin.com/in/jacobpatterson1549">LinkedIn</a></li>
         </ul>
     </div>
