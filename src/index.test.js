6dbd06e23743b40fccdd3534d307d626ca9c527a
Jacob Patterson <jacob.patterson@gmail.com>
2021-05-21 17:10:50 -0700

    adding basic https redirection

--- src/index.test.js
+++ src/index.test.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 jest.mock('react-dom', () => ({ render: jest.fn() }));
 
 describe('index', () => {
-    it('should render on the root element', () => {
+    it('should redirect to https and render on the root element', () => {
+        window.location.protocol = 'http:';
         const rootElement = document.createElement('div');
         rootElement.setAttribute('id', 'root');
         document.body.appendChild(rootElement);
         require('./index.js');
+        expect(window.location.protocol).toBe('https:');
         expect(ReactDOM.render).toHaveBeenCalledWith(expect.anything(), rootElement);
     });
 });
