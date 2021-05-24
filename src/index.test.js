6f0be02cb92eff58f551085b68155cc6255e47f6
Jacob Patterson <jacob.patterson@gmail.com>
2021-05-24 11:40:03 -0700

    improving test

--- src/index.test.js
+++ src/index.test.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 jest.mock('react-dom', () => ({ render: jest.fn() }));
 
 describe('index', () => {
-    it('should redirect to https and render on the root element', () => {
-        window.location.protocol = 'http:';
+    afterEach(() => {
+        jest.resetModules();
+    });
+    it('should render on the root element', () => {
         const rootElement = document.createElement('div');
         rootElement.setAttribute('id', 'root');
         document.body.appendChild(rootElement);
         require('./index.js');
-        expect(window.location.protocol).toBe('https:');
         expect(ReactDOM.render).toHaveBeenCalledWith(expect.anything(), rootElement);
+    });
+    describe('window.location.protocol', () => {
+        it.each(['http:', 'https:', 'ftp:'])('should redirect to https when protocol is %s', (protocol) => {
+            window.location.protocol = protocol;
+            require('./index.js');
+            expect(window.location.protocol).toBe('https:');
+        });
     });
 });
