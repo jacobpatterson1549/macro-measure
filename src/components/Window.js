b752593790f94e78c4073f5eda0c0f565748d4e6
Jacob Patterson <jacob.patterson@gmail.com>
2021-05-24 10:34:36 -0700

    adding Window component
     Window listens to events to provide properties in a Render Prop component

---
+++ src/components/Window.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+
+const isFullscreen = () => !!document.fullscreenElement;
+
+// Window listens to events to provide properties in a Render Prop component
+export const Window = ({ render }) => {
+
+    const [fullscreen, setFullscreen] = useState(isFullscreen());
+    const fullscreenChanged = () => setFullscreen(isFullscreen());
+    useEffect(() => {
+        window.addEventListener('fullscreenchange', fullscreenChanged);
+        return () => {
+            window.removeEventListener('fullscreenchange', fullscreenChanged)
+        };
+    });
+
+    const [onLine, setOnLine] = useState(window.navigator.onLine);
+    const goOnline = () => setOnLine(true);
+    const goOffline = () => setOnLine(false);
+    useEffect(() => {
+        window.addEventListener('online', goOnline);
+        window.addEventListener('offline', goOffline);
+        return () => {
+            window.removeEventListener('online', goOnline);
+            window.removeEventListener('offline', goOffline);
+        };
+    }, []);
+
+    const [promptInstall, setPromptInstall] = useState(null);
+    const handler = (event) => {
+        event.preventDefault();
+        const fn = promptInstallFn(event);
+        setPromptInstall(fn);
+    };
+    useEffect(() => {
+        window.addEventListener('beforeinstallprompt', handler);
+        return () => {
+            window.removeEventListener('beforeinstallprompt', handler);
+        };
+    });
+    const promptInstallFn = (installPromptEvent) => async () => {
+        installPromptEvent.prompt();
+        const choiceResult = await installPromptEvent.userChoice();
+        if (choiceResult.outcome === 'accepted') {
+            setPromptInstall(null);
+        }
+    };
+
+    return (
+        <>
+            {render({
+                fullscreen: fullscreen,
+                onLine: onLine,
+                promptInstall: promptInstall,
+            })}
+        </>
+    );
+};
---
+++ src/components/Window.test.js
@@ -0,0 +1,104 @@
+import { render, screen, fireEvent, createEvent, waitFor } from '@testing-library/react';
+
+import { Window } from './Window';
+
+describe('Window', () => {
+    const MockApp = ({ win }) => {
+        return (
+            <div>
+                <p title="fullscreen">{String(win.fullscreen)}</p>
+                <p title="onLine">{String(win.onLine)}</p>
+                {
+                    win.promptInstall &&
+                    <button onClick={async () => await win.promptInstall}>INSTALL</button>
+                }
+            </div>
+        );
+    };
+    const tests = [
+        [true, true],
+        [true, false],
+        [false, true],
+        [false, false],
+    ];
+    describe('fullscreen', () => {
+        it.each(tests)('should initially be fullscreen=%s then transition to fullscreen=%s', (initial, transition) => {
+            document.fullscreenElement = initial ? {} : null;
+            render(<Window render={win => (<MockApp win={win} />)} />);
+            const element = screen.getByTitle('fullscreen');
+            expect(element.textContent).toBe(String(initial));
+            const event = createEvent('fullscreenchange', window);
+            document.fullscreenElement = transition ? {} : null;
+            fireEvent(window, event);
+            expect(element.textContent).toBe(String(transition));
+        });
+    });
+    describe('onLine', () => {
+        it.each(tests)('should initially be onLine=%s then transition to onLine=%s', (initial, transition) => {
+            window.navigator.onLine = initial;
+            render(<Window render={win => (<MockApp win={win} />)} />);
+            const element = screen.getByTitle('onLine');
+            expect(element.textContent).toBe(String(initial));
+            const event = createEvent(transition ? 'online' : 'offline', window);
+            fireEvent(window, event);
+            expect(element.textContent).toBe(String(transition));
+        });
+    });
+    describe('promptInstall', () => {
+        const mockOnbeforeinstallprompt = (accepted) => {
+            // test procedure from https://github.com/testing-library/testing-library-docs/issues/798
+            const event = createEvent('beforeinstallprompt', window);
+            Object.defineProperties(event, {
+                preventDefault: { value: jest.fn() },
+                userChoice: { value: jest.fn().mockResolvedValue({ outcome: accepted }) },
+                prompt: { value: jest.fn() },
+            });
+            fireEvent(window, event);
+            return event
+        };
+        it('should not have the install button when no beforeinstallprompt event is fired', () => {
+            render(<Window render={win => (<MockApp win={win} />)} />);
+            const element = screen.queryByRole('button');
+            expect(element).not.toBeInTheDocument();
+        });
+        it('should have the install button when the event is fired', () => {
+            render(<Window render={win => (<MockApp win={win} />)} />);
+            mockOnbeforeinstallprompt();
+            const element = screen.queryByRole('button');
+            expect(element).toBeInTheDocument();
+        });
+        it('should preventDefault when the event is fired', () => {
+            render(<Window render={win => (<MockApp win={win} />)} />);
+            const event = mockOnbeforeinstallprompt();
+            expect(event.preventDefault).toBeCalled();
+        });
+        it('should call prompt when the install button is clicked', async () => {
+            render(<Window render={win => (<MockApp win={win} />)} />);
+            const event = mockOnbeforeinstallprompt();
+            const element = screen.queryByRole('button');
+            await waitFor(() => fireEvent.click(element));
+            expect(event.prompt).toBeCalled();
+        });
+        it('should call userChoice when the install button is clicked', async () => {
+            render(<Window render={win => (<MockApp win={win} />)} />);
+            const event = mockOnbeforeinstallprompt();
+            const element = screen.queryByRole('button');
+            await waitFor(() => fireEvent.click(element));
+            expect(event.userChoice).toBeCalled();
+        });
+        it('should remove install button from the document when "accepted" is the choiceResult of the prompt', async () => {
+            render(<Window render={win => (<MockApp win={win} />)} />);
+            mockOnbeforeinstallprompt('accepted');
+            const element = screen.queryByRole('button');
+            await waitFor(() => fireEvent.click(element));
+            expect(element).not.toBeInTheDocument();
+        });
+        it.each(['dismissed', 'not accepted', null])('should keep install button in the document when "%s" is the choiceResult of the prompt', async (accepted) => {
+            render(<Window render={win => (<MockApp win={win} />)} />);
+            mockOnbeforeinstallprompt(accepted);
+            const element = screen.queryByRole('button');
+            await waitFor(() => fireEvent.click(element));
+            expect(element).toBeInTheDocument();
+        });
+    });
+});
