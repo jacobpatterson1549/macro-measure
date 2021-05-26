301980bd4d09bfd42f4f9a272f6378a8a664b3fd
Jacob Patterson <jacob.patterson@gmail.com>
2021-05-26 12:47:50 -0700

    better mock definitions

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,25 +4,37 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-const localStorageMock = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    removeItem: jest.fn(),
-    clear: jest.fn(),
-    key: jest.fn(),
-    length: 0,
-};
-const locationMock = {
-    assign: jest.fn(),
-    reload: jest.fn(),
-    protocol: 'https:',
-    hostname: 'macro-measure.herokuapp.com',
-};
-const geolocationMock = {
-    watchPosition: jest.fn(),
-    clearWatch: jest.fn(),
-};
-Object.defineProperty(global, 'localStorage', { value: localStorageMock });
-Object.defineProperty(global, 'location', { value: locationMock, writable: true });
-Object.defineProperty(global.navigator, 'geolocation', { value: geolocationMock, writable: true });
-Object.defineProperty(global.navigator, 'onLine', { value: true, writable: true });
+Object.defineProperties(global, {
+    'localStorage': {
+        value: {
+            getItem: jest.fn(),
+            setItem: jest.fn(),
+            removeItem: jest.fn(),
+            clear: jest.fn(),
+            key: jest.fn(),
+            length: 0,
+        },
+    },
+    'location': {
+        value: {
+            assign: jest.fn(),
+            reload: jest.fn(),
+            protocol: 'https:',
+            hostname: 'macro-measure.herokuapp.com',
+        },
+        writable: true,
+    },
+});
+Object.defineProperties(global.navigator, {
+    'geolocation': {
+        value: {
+            watchPosition: jest.fn(),
+            clearWatch: jest.fn(),
+        },
+        writable: true,
+    },
+    'onLine': {
+        value: true,
+        writable: true,
+    },
+});
