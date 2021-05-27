bfbc4d681d1b030032d85a6e2accd1f0c6ad9e9f
Jacob Patterson <jacob.patterson@gmail.com>
2021-05-27 08:37:43 -0700

    using public url in sw url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { App } from './components/App';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    const swURL = `${process.env.PUBLIC_URL}/service-worker.js`;
+    navigator.serviceWorker.register(swURL)
       .then((swr) => console.log('service worker registered:', swr.scope))
       .catch((err) => console.log('service worker did not register:', err.message ? err.message : null));
   });
