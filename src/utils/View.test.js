b5199753445ceb985f7140e407ff10b70a956e79
Jacob Patterson <jacob.patterson@gmail.com>
2021-05-30 13:15:08 -0700

    using array.forEach instead of map

--- src/utils/View.test.js
+++ src/utils/View.test.js
@@ -3,7 +3,7 @@ import { View } from './View';
 describe('View', () => {
   it('should have have no duplicates', () => {
     const views = {};
-    Object.values(View).map((view) => {
+    Object.values(View).forEach((view) => {
       expect(views[view]).toBeFalsy();
       views[view] = true;
     });
