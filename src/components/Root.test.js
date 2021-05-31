f7eef7baa7165eddea96d57fa40a8e84542494ef
Jacob Patterson <jacob.patterson@gmail.com>
2021-05-31 11:30:43 -0700

    removing unused import

--- src/components/Root.test.js
+++ src/components/Root.test.js
@@ -2,8 +2,6 @@ import { render, screen } from '@testing-library/react';
 
 import { Root } from './Root';
 
-import { View } from '../utils/View';
-
 describe('Root', () => {
   it('should have help header in the document', () => {
     render(<Root />);
