3ac7ccb29ee3ef7199e2a7859f89e6ff96fc8f3d
Jacob Patterson <jacob.patterson@gmail.com>
2021-06-01 07:56:59 -0700

    better view enum
    The values are not defined in two places.  The enum objects are mapped to id by name and flags by id for qucik lookups.  The ids could be omitted, using the index+1 second parameter to the .map() operation, but that makes the code much move confusing.

--- src/utils/View.js
+++ src/utils/View.js
@@ -5,43 +5,38 @@ const DELETE = 1 << 3;
 const NEEDS_GPS = 1 << 4;
 const HAS_GROUP = 1 << 5;
 
-const views = {
-    About: 1,
-    Help: 2,
-    Settings: 3,
-    Item_Create: 4,
-    Item_Read: 5,
-    Item_Read_List: 6,
-    Item_Update: 7,
-    Item_Delete: 8,
-    Group_Create: 9,
-    Group_Read_List: 10,
-    Group_Update: 11,
-    Group_Delete: 12,
+const views = { // name: [id, flags]
+    About: [1],
+    Help: [2],
+    Settings: [3],
+    Item_Create: [4, CREATE | HAS_GROUP | NEEDS_GPS],
+    Item_Read: [5, READ | HAS_GROUP | NEEDS_GPS],
+    Item_Read_List: [6, READ | HAS_GROUP],
+    Item_Update: [7, UPDATE | HAS_GROUP],
+    Item_Delete: [8, DELETE | HAS_GROUP],
+    Group_Create: [9, CREATE],
+    Group_Read_List: [10, READ],
+    Group_Update: [11, UPDATE],
+    Group_Delete: [12, DELETE],
 };
 
-const flags = {
-    [views.About]: 0,
-    [views.Help]: 0,
-    [views.Settings]: 0,
-    [views.Item_Create]: CREATE | HAS_GROUP | NEEDS_GPS,
-    [views.Item_Read]: READ | HAS_GROUP | NEEDS_GPS,
-    [views.Item_Update]: UPDATE | HAS_GROUP,
-    [views.Item_Delete]: DELETE | HAS_GROUP,
-    [views.Item_Read_List]: READ | HAS_GROUP,
-    [views.Group_Create]: CREATE,
-    [views.Group_Update]: UPDATE,
-    [views.Group_Delete]: DELETE,
-    [views.Group_Read_List]: READ,
-};
+const viewIdsByName = Object.fromEntries(
+    Object.entries(views)
+        .map(([name, view]) => [name, view[0]]));
+
+const viewFlagsById = Object.fromEntries(
+    Object.entries(views)
+        .map(([_, view]) => [view[0], view[1]]));
 
 const viewFuncs = {
-    isCreate: (view) => !!(flags[view] & CREATE),
-    isRead: (view) => !!(flags[view] & READ),
-    isUpdate: (view) => !!(flags[view] & UPDATE),
-    isDelete: (view) => !!(flags[view] & DELETE),
-    hasGroup: (view) => !!(flags[view] & HAS_GROUP),
-    needsGPS: (view) => !!(flags[view] & NEEDS_GPS),
+    isCreate: (viewId) => !!(viewFlagsById[viewId] & CREATE),
+    isRead: (viewId) => !!(viewFlagsById[viewId] & READ),
+    isUpdate: (viewId) => !!(viewFlagsById[viewId] & UPDATE),
+    isDelete: (viewId) => !!(viewFlagsById[viewId] & DELETE),
+    hasGroup: (viewId) => !!(viewFlagsById[viewId] & HAS_GROUP),
+    needsGPS: (viewId) => !!(viewFlagsById[viewId] & NEEDS_GPS),
 };
 
-export const View = Object.assign({}, views, viewFuncs);
+export const View = Object.assign({},
+    viewIdsByName,
+    viewFuncs);
