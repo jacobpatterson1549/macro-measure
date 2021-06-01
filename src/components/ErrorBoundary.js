702b6549f5f5063cc38d46c47fca5f7f26c0587b
Jacob Patterson <jacob.patterson@gmail.com>
2021-06-01 07:16:36 -0700

    making ErrorBoundary more functional
    It must be a class to work, but using functional syntax to override methods.
    Removing hasState variable, checking the truthyness of the error

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -3,31 +3,24 @@ import { Component } from 'react';
 import { LocalStorageSettings } from './LocalStorageSettings';
 
 export class ErrorBoundary extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false, error: null };
-  }
 
-  static getDerivedStateFromError(error) {
-    return {
-      hasError: true,
-      error: error,
-    };
-  }
+  state = { error: null };
 
-  render() {
-    return (!this.state.hasError)
-      ? this.props.children
-      : (
-        <div>
-          <h1>Something went wrong.</h1>
-          {
-            this.state.error.message &&
-            <h3>{this.state.error.message}</h3>
-          }
-          <p>Deleting or importing saved data may fix the problem.</p>
-          <LocalStorageSettings />
-        </div>
-      );
-  }
-}
+  static getDerivedStateFromError = (error) => (
+    { error }
+  );
+
+  render = () => (!this.state.error)
+    ? this.props.children
+    : (
+      <div>
+        <h1>Something went wrong.</h1>
+        {
+          this.state.error.message &&
+          <h3>{this.state.error.message}</h3>
+        }
+        <p>Deleting or importing saved data may fix the problem.</p>
+        <LocalStorageSettings />
+      </div>
+    );
+};
