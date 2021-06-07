b531ba8c1604e1e497c52d82829a22c17e2d217f
Jacob Patterson <jacob.patterson@gmail.com>
2021-06-07 14:51:11 -0700

    renaming vars to show that view ID is param

--- src/utils/View.test.js
+++ src/utils/View.test.js
@@ -4,22 +4,22 @@ describe('View', () => {
   const views = Object.values(View).filter(Number.isInteger);
   it('should have no duplicate ids', () => {
     const ids = {};
-    views.forEach((view) => {
-      expect(ids[view]).toBeFalsy();
-      ids[view] = true;
+    views.forEach((viewId) => {
+      expect(ids[viewId]).toBeFalsy();
+      ids[viewId] = true;
     });
   });
   it('should not have view with an id of zero', () => {
-    const zeroIdViews = views.filter((view) => view === 0);
+    const zeroIdViews = views.filter((viewId) => viewId === 0);
     expect(zeroIdViews.length).toBe(0);
   });
-  it.each(views)('should have desired flags when view is id:%s', (view) => {
-    expect(View.isCreate(view)).toBe([View.Group_Create, View.Item_Create].includes(view));
-    expect(View.isRead(view)).toBe([View.Group_Read_List, View.Item_Read_List, View.Item_Read].includes(view));
-    expect(View.isUpdate(view)).toBe([View.Group_Update, View.Item_Update].includes(view));
-    expect(View.isDelete(view)).toBe([View.Group_Delete, View.Item_Delete].includes(view));
-    expect(View.needsGPS(view)).toBe([View.Item_Read, View.Item_Create].includes(view));
-    expect(View.isItem(view)).toBe([View.Item_Create, View.Item_Read, View.Item_Read_List, View.Item_Update, View.Item_Delete].includes(view));
-    expect(View.isGroup(view)).toBe([View.Group_Create, View.Group_Read_List, View.Group_Update, View.Group_Delete].includes(view));
+  it.each(views)('should have desired flags when view is id:%s', (viewId) => {
+    expect(View.isCreate(viewId)).toBe([View.Group_Create, View.Item_Create].includes(viewId));
+    expect(View.isRead(viewId)).toBe([View.Group_Read_List, View.Item_Read_List, View.Item_Read].includes(viewId));
+    expect(View.isUpdate(viewId)).toBe([View.Group_Update, View.Item_Update].includes(viewId));
+    expect(View.isDelete(viewId)).toBe([View.Group_Delete, View.Item_Delete].includes(viewId));
+    expect(View.needsGPS(viewId)).toBe([View.Item_Read, View.Item_Create].includes(viewId));
+    expect(View.isItem(viewId)).toBe([View.Item_Create, View.Item_Read, View.Item_Read_List, View.Item_Update, View.Item_Delete].includes(viewId));
+    expect(View.isGroup(viewId)).toBe([View.Group_Create, View.Group_Read_List, View.Group_Update, View.Group_Delete].includes(viewId));
   })
 });
