d1e05816522763fd6e0f5cf2401f74cec9ceacb6
Jacob Patterson <jacob.patterson@gmail.com>
2021-06-08 07:42:10 -0700

    making service worker code more functional
    formatting: using single quotes

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,50 +1,44 @@
 const cacheName = '%CACHE_NAME%';
 
 const assets = [
-    "./?pwa",
-    "./favicon.ico",
-    "./favicon.svg",
-    "./favicon192.png",
-    "./favicon512.png",
-    "./manifest.json",
-    "./robots.txt",
-    "./static/js/bundle.js",
-    "./static/js/main.chunk.js",
-    "./static/js/vendors~main.chunk.js",
+    './?pwa',
+    './favicon.ico',
+    './favicon.svg',
+    './favicon192.png',
+    './favicon512.png',
+    './manifest.json',
+    './robots.txt',
+    './static/js/bundle.js',
+    './static/js/main.chunk.js',
+    './static/js/vendors~main.chunk.js',
 ]; // %ASSET_MANIFEST%
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => (
     event.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll(assets);
-        })
-        .catch(error => {
-            throw new Error("install: adding assets to cache failed: " + error);
-        })
-    );
-});
+        caches.open(cacheName).then(cache => (
+            cache.addAll(assets)
+        ))
+    )
+));
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => (
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-        .catch(error => {
-            throw new Error("fetch: failed to get " + event.request.url + ": " + error);
-        })
-    );
-});
+        caches.match(event.request).then(response => (
+            response || fetch(event.request)
+        ))
+    )
+));
 
-self.addEventListener("activate", event => {
+self.addEventListener('activate', event => (
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(name => {
-                    if (name !== cacheName) {
-                        return caches.delete(name);
-                    }
-                })
-            );
-        })
-    );
-});
+        caches.keys().then(cacheNames => (
+            Promise.all(
+                cacheNames
+                    .filter(name => name !== cacheName)
+                    .map(name => (
+                        caches.delete(name)
+                    ))
+            )
+        ))
+    )
+));
--- scripts/generate-sw.js
+++ scripts/generate-sw.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const package = require("../package.json");
-const assetManifest = require("../build/asset-manifest.json");
+const package = require('../package.json');
+const assetManifest = require('../build/asset-manifest.json');
 const swPath = 'build/service-worker.js';
 
 const cacheName = `cache-${package.version}`;
@@ -9,21 +9,22 @@ const manifestAssets = JSON.stringify(
     assetManifest.entrypoints
         .map(entrypoint => `./${entrypoint}`));
 
-fs.readFile(swPath, 'utf8', (err, oldSW) => {
-    if (err) {
-        throw new Error(`reading service worker: ${err.message}`);
-    }
-
-    console.log(`setting service worker cache name to ${cacheName}\n`);
-    console.log(`appending service worker assets with ${manifestAssets}\n`);
-    const newSW = oldSW
+const updateServiceWorker = (data) => (
+    data
         .replace('%CACHE_NAME%', cacheName)
-        .replace(']; // %ASSET_MANIFEST%', `].concat(${manifestAssets});`);
+        .replace(']; // %ASSET_MANIFEST%', `].concat(${manifestAssets});`)
+);
 
-    fs.writeFile(swPath, newSW, 'utf8', (err) => {
-        if (err) {
-            throw new Error(`writing service worker: ${err.message}`);
-        }
-        console.log('[generated service worker]')
-    });
-});
+const throwError = (message) => {
+    throw new Error(message);
+};
+
+fs.readFile(swPath, 'utf8', (err, data) => (
+    err
+        ? throwError(`reading service worker: ${err.message}`)
+        : fs.writeFile(swPath, updateServiceWorker(data), 'utf8', (err) => (
+            err
+                ? throwError(`writing service worker: ${err.message}`)
+                : console.log('[generated service worker]')
+        ))
+));
