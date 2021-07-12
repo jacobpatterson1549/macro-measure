b81c99126810908e3f27c8c98bbc14f557e91c86
Jacob Patterson <jacob.patterson@gmail.com>
2021-07-12 09:54:33 -0700

    removing unneeded test

--- src/utils/Global.test.js
+++ src/utils/Global.test.js
@@ -1,15 +1,6 @@
-import { isFullscreen, getCurrentDate } from './Global';
+import { getCurrentDate } from './Global';
 
 describe('Global', () => {
-    const isFullscreenTests = [
-        [{}, true],
-        [null, false],
-    ];
-    it.each(isFullscreenTests)('should cast fullscreenElement of the document (%s) as a bool (%s)', (fullscreenElement, expected) => {
-        window.document.fullscreenElement = fullscreenElement;
-        const actual = isFullscreen();
-        expect(actual).toBe(expected);
-    });
     const getCurrentDateTests = [
         ['should have contain digits and UTC (Zulu) timezone (Z)', Date.UTC(2021, 5, 1, 17, 22, 30, 554), '20210601172230554Z'],
         ['should end in Z even if date is not in GMT', Date.parse('Tue Jun 01 2021 12:36:26 GMT-0700'), '20210601193626000Z'],
